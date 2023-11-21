899172a5cb11081a137ecbec679ebb35f7358f75
Secretny <ataszymon@gmail.com>
2023-11-21 20:50:34 +0100

    zadanie 5 i helpery

---
+++ Zestaw 5/5.py
@@ -0,0 +1,50 @@
+import math
+import random
+from helpers import is_prime
+
+
+def check(T):
+    print(T)
+    if T[-1] == 0:
+        return False
+    for i in range(len(T) - 1):
+        start_frag = [1 for _ in range(i + 2)]
+        check_fragment(T, start_frag)
+
+
+def check_fragment(T, fragment):
+    if sum(fragment) == len(T):
+        check_primes(T, fragment)
+        return 0
+    fragment[0] += 1
+    check_fragment(T, fragment)
+    k = 0
+    for i in range(1, len(fragment)):
+        if fragment[i] < 30:
+            fragment[i] += 1
+            fragment[i - 1 - k] -= 1
+            k = 0
+        else:
+            k += 1
+        check_fragment(T, fragment)
+
+
+def check_primes(T, fragment):
+    index = 0
+    for v in fragment:
+        if not is_prime(join_numbers(T[index:v + index])):
+            return False
+        index += v
+    print(True)
+
+
+def join_numbers(T):
+    s = 0
+    for i in range(-1, -len(T) - 1, -1):
+        s += T[i] * 10 ** (abs(i) - 1)
+    return s
+
+
+t = [1] + [math.floor(random.random() * 2) for _ in range(random.randint(3, 20))]
+
+print(check([1, 1, 0, 0, 1, 1]))
---
+++ Zestaw 5/helpers.py
@@ -0,0 +1,12 @@
+import math
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n) + 1), 2):
+        if n % i == 0:
+            return False
+    return True
