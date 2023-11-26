aadfa0ad27fd39ef703bb92493df47503f361e6a
Secretny <ataszymon@gmail.com>
2023-11-26 22:10:21 +0100

    dodano

---
+++ Zestaw 5/11.py
@@ -0,0 +1,24 @@
+# Zadanie 11.
+# Dana jest tablica T[N]. Prosze napisac program zliczajacy liczbe “enek” o okreslonym iloczynie.
+from helpers import *
+
+
+def enki(T, prod):
+    result = 0
+
+    def search(T, prod, index=0, cur_prod=1):
+        nonlocal result
+        if cur_prod * T[index] == prod:
+            result += 1
+            return 0
+        if index != len(T) - 1:
+            search(T, prod, index + 1, cur_prod)
+            search(T, prod, index + 1, cur_prod * T[index])
+
+    search(T, prod)
+    return result
+
+
+Tab = random_linear_list(20, 15)
+print(Tab)
+print(enki(Tab, 36))
