3674951a1bd34b71eb899c0fc27de871a1b4f2f1
Secretny <ataszymon@gmail.com>
2023-11-28 21:07:32 +0100

    dodano nowe zadania

--- Zestaw 5/17.py
+++ Zestaw 5/17.py
@@ -16,14 +16,14 @@ def remove_first_digit(n):
 def merge_numbers(a, b):
     counter = 0
 
-    def check(n1, n2, k=0):
+    def check(n1, n2, curr_n=0):
         nonlocal counter
-        if is_prime(k):
+        if is_prime(curr_n):
             counter += 1
         if n1 != 0:
-            check(remove_first_digit(n1), n2, 10 * k + get_first_digit(n1))
+            check(remove_first_digit(n1), n2, 10 * curr_n + get_first_digit(n1))
         if n2 != 0:
-            check(n1, remove_first_digit(n2), 10 * k + get_first_digit(n2))
+            check(n1, remove_first_digit(n2), 10 * curr_n + get_first_digit(n2))
 
     check(a, b)
     return counter
