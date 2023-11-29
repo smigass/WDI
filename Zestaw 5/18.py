0af6b53a2bced01aa6b40a823a18bd6b34bee48c
Secretny <ataszymon@gmail.com>
2023-11-29 23:38:13 +0100

    update

--- Zestaw 5/18.py
+++ Zestaw 5/18.py
@@ -8,46 +8,42 @@
 
 import math
 
-from helpers import random_list, listuj
+from helpers import random_list, list_table
 
-
-# board = random_list(8, 50)
-# distance = [[max(abs(i - 7), abs(j - 7)) for j in range(8)] for i in range(8)]
-# listuj(board)
 
 # TODO - make it go the other way (from the bottom-right corner to position (w, k))
 def can_move(prev, new, history):
-    p, n = board[prev[0]][prev[1]], board[new[0]][new[1]]
-    last_prev = p % 10
-    first_new = n // 10 ** (int(math.log10(n)))
-    return last_prev < first_new and new not in history
+    if distance[prev[0]][prev[1]] >= distance[new[0]][new[1]]:
+        p, n = board[prev[0]][prev[1]], board[new[0]][new[1]]
+        last_prev = p % 10
+        first_new = n // 10 ** (int(math.log10(n)))
+        return last_prev < first_new and new not in history
+    return False
 
 
 min_dist = 100
 
 
-def check(w, k, prev=[]):
+def check(w, k, prev=None):
+    if prev is None:
+        prev = []
     dist = distance[w][k]
     global min_dist
     if dist < min_dist:
         min_dist = dist
     if dist == 0:
         return True
-    best_result = False
     for i in range(-1, 2):
         for j in range(-1, 2):
             if 0 <= w + i <= 7 and 0 <= k + j <= 7:
                 if can_move((w, k), (w + i, k + j), prev):
                     if i != 0 or j != 0:
-                        best_result = check(w + i, k + j, [*prev, (w, k)])
-                        break
-    return best_result
+                        if check(w + i, k + j, [*prev, (w, k)]):
+                            return True
+    return False
 
 
-while True:
-    board = random_list(8, 50)
-    distance = [[max(abs(i - 7), abs(j - 7)) for j in range(8)] for i in range(8)]
-    listuj(board)
-    print(min_dist)
-    if check(3, 3):
-        break
+board = random_list(8, 50)
+distance = [[max(abs(i - 7), abs(j - 7)) for j in range(8)] for i in range(8)]
+list_table(board)
+print(check(5,7))
