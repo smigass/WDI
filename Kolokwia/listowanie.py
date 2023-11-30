9508dcc17b593b6b2462bdc24960a2a91a4d7634
Secretny <ataszymon@gmail.com>
2023-11-30 21:10:41 +0100

    update

--- Kolokwia/listowanie.py
+++ Kolokwia/listowanie.py
@@ -2,8 +2,11 @@ import random
 
 
 def listuj(T):
+    print()
     for i in range(len(T)):
-        print(T[i])
+        for j in range(len(T)):
+            print(T[i][j], end='\t')
+        print('')
     print()
 
 def random_list(n, a):
