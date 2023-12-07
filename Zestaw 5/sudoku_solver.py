f5bce517d7b86e556637970d10274ecd2238f85a
Secretny <ataszymon@gmail.com>
2023-12-07 20:40:36 +0100

    update

--- Zestaw 5/sudoku_solver.py
+++ Zestaw 5/sudoku_solver.py
@@ -2,53 +2,9 @@ from helpers import list_table
 from sudoku_generator import Sudoku
 
 
-def sudokuCheck(grid):
-    # edge cases
-    if not grid:
-        return False
-
-    # rows by rows checking
-    hset = set()
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j] in hset:
-                return False
-            else:
-                hset.add(grid[i][j])
-        hset = set()
-
-    # cols by cols checking
-    hset = set()
-    for i in range(9):
-        for j in range(9):
-            if grid[j][i] in hset:
-                return False
-            else:
-                hset.add(grid[j][i])
-        hset = set()
-
-    # 3 by 3 check
-    subs = [range(0, 3), range(3, 6), range(6, 9)]
-    subgrids = []
-    for x in subs:
-        for y in subs:
-            subgrids.append([x, y])
-    for (row_range, column_range) in subgrids:
-        hset = set()
-        for i in row_range:
-            for j in column_range:
-                if grid[i][j] in hset:
-                    return False
-                else:
-                    hset.add(grid[i][j])
-
-    return True
-
-
 def sudoku_solver(tab, empty_tab, index=0):
     if index == len(empty_tab):
         list_table(tab)
-        print(sudokuCheck(tab))
     else:
         row = empty_tab[index][0]
         col = empty_tab[index][1]
