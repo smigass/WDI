11d58c90d79f038c84d235af4674a26ae23b89fb
Secretny <ataszymon@gmail.com>
2023-12-10 23:57:37 +0100

    update

--- Kolokwia/A4_2022.py
+++ Kolokwia/A4_2022.py
@@ -7,24 +7,27 @@ empty = "○"
 checked = "x"
 
 N = 9
-T = [[empty if random.random() < 0.95 else knight for _ in range(N)] for _ in range(N)]
+T = [[empty if random.random() < 0.90 else knight for _ in range(N)] for _ in range(N)]
 listuj(T)
 
-def check_checks(T):
+
+# Funkcja find_checks zaznacza w tablicy poczatkowej pola zaszachowane przez
+# poczatkowe ustawienie skoczkow (zaznacza puste pole "○" ktore sa szachowane na "x")
+def find_checks(tab):
     possible_checks = [(1, 2), (2, 1), (1, -2), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
-    for i in range(len(T)):
-        for j in range(len(T)):
-            if T[i][j] == knight:
+    for i in range(len(tab)):
+        for j in range(len(tab)):
+            if tab[i][j] == knight:
                 for p in possible_checks:
                     virtual_move = i + p[0], j + p[1]
-                    if 0 <= virtual_move[0] < len(T) and 0 <= virtual_move[1] < len(T):
-                        if T[virtual_move[0]][virtual_move[1]] != knight:
-                            T[virtual_move[0]][virtual_move[1]] = checked
-    return T
+                    if 0 <= virtual_move[0] < len(tab) and 0 <= virtual_move[1] < len(tab):
+                        if tab[virtual_move[0]][virtual_move[1]] != knight:
+                            tab[virtual_move[0]][virtual_move[1]] = checked
+    return tab
 
 
 def place(t):
-    t = check_checks(t)
+    t = find_checks(t)
     listuj(t)
     n, min_dist = len(t), len(t)
     middle = (n / 2, n / 2)
@@ -33,7 +36,8 @@ def place(t):
     possible_checks = [(1, 2), (2, 1), (1, -2), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
     for row in range(n):
         for col in range(n):
-            if t[row][col] == knight: continue
+            if t[row][col] == knight:
+                continue
             dist = max(abs(middle[0] - row), abs(middle[1] - col))
             new = 0
             for p in possible_checks:
