7e3584ae615c9b668c3df3b44062a9a6af977668
Secretny <ataszymon@gmail.com>
2023-12-10 23:30:29 +0100

    update

--- Kolokwia/A4_2022.py
+++ Kolokwia/A4_2022.py
@@ -1,8 +1,18 @@
-import random
-
 from listowanie import listuj
 
-T = [[0 if random.random() < 0.85 else 1 for _ in range(8)] for _ in range(8)]
+# TODO - Do poprawy
+N = 9
+# T = [[0 if random.random() < 0.85 else 1 for _ in range(N)] for _ in range(N)]
+T = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
 listuj(T)
 
 
@@ -14,12 +24,13 @@ def place(t):
     possible_checks = [(1, 2), (2, 1), (1, -2), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
     for row in range(n):
         for col in range(n):
+            if t[row][col] == 1: continue
             dist = max(abs(middle[0] - row), abs(middle[1] - col))
             new = 0
             for p in possible_checks:
-                next = row + p[0], col + p[1]
-                if 0 <= next[0] < n and 0 <= next[1] < n:
-                    if t[next[0]][next[1]] == 0:
+                virtual_move = row + p[0], col + p[1]
+                if 0 <= virtual_move[0] < n and 0 <= virtual_move[1] < n:
+                    if t[virtual_move[0]][virtual_move[1]] == 0:
                         new += 1
             if new > max_new:
                 max_new = new
@@ -28,10 +39,6 @@ def place(t):
                 min_dist = dist
                 best = row, col
     return best
-
-
-
-
 
 
 print(place(T))
