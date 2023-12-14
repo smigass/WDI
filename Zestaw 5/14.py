975ef3141014a261ed388960b163d09e819425d1
Secretny <ataszymon@gmail.com>
2023-12-14 11:54:32 +0100

    added

--- Zestaw 5/14.py
+++ Zestaw 5/14.py
@@ -1,3 +1,9 @@
+# Zadanie 14. Problem wiez w Hanoi (tresc oczywista)
 def hanoi(n, a, b, c):
     if n > 0:
-        hanoi(n - 1, )
+        hanoi(n - 1, a, c, b)
+        print(f'{a} ➞ {c}')
+        hanoi(n - 1, b, a, c)
+
+
+hanoi(4, "A", "B", "C")
